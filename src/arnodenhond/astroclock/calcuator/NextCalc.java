39b398fed7d7c5bf010ef732ffa6dd074a8b17e1
arnodenhond <arnodenhond@gmail.com>
2012-06-19 23:19:58 +0200

    solstice and equinox times in utc

--- src/arnodenhond/astroclock/calcuator/NextCalc.java
+++ src/arnodenhond/astroclock/calcuator/NextCalc.java
@@ -175,6 +175,7 @@ public class NextCalc {
 		int[] hours = new int[] { 5, 11, 17, 23, 4, 10, 16 };
 		int[] minutes = new int[] { 30, 11, 11, 3, 48, 44, 28 };
 		Calendar c = Calendar.getInstance();
+		c.setTimeZone(TimeZone.getTimeZone("UTC"));
 		int y = c.get(Calendar.YEAR) - 2011;
 		c.set(Calendar.MONTH, 11);
 		c.set(Calendar.DATE, days[y]);
@@ -197,6 +198,7 @@ public class NextCalc {
 		int[] hours = new int[] { 17, 23, 5, 10, 16, 22, 4 };
 		int[] minutes = new int[] { 16, 9, 4, 51, 38, 34, 24 };
 		Calendar c = Calendar.getInstance();
+		c.setTimeZone(TimeZone.getTimeZone("UTC"));
 		int y = c.get(Calendar.YEAR) - 2011;
 		c.set(Calendar.MONTH, 5);
 		c.set(Calendar.DATE, days[y]);
@@ -219,6 +221,7 @@ public class NextCalc {
 		int[] hours = new int[] { 9, 14, 20, 2, 8, 14, 20 };
 		int[] minutes = new int[] { 4, 49, 44, 29, 20, 21, 2 };
 		Calendar c = Calendar.getInstance();
+		c.setTimeZone(TimeZone.getTimeZone("UTC"));
 		int y = c.get(Calendar.YEAR) - 2011;
 		c.set(Calendar.MONTH, 8);
 		c.set(Calendar.DATE, days[y]);
@@ -241,6 +244,7 @@ public class NextCalc {
 		int[] hours = new int[] { 23, 5, 11, 16, 22, 4, 10 };
 		int[] minutes = new int[] { 21, 14, 2, 57, 45, 30, 28 };
 		Calendar c = Calendar.getInstance();
+		c.setTimeZone(TimeZone.getTimeZone("UTC"));
 		int y = c.get(Calendar.YEAR) - 2011;
 		c.set(Calendar.MONTH, 2);
 		c.set(Calendar.DATE, days[y]);
